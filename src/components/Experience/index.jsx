12f2d57010cd04d3180716aa95304d394e307947
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2025-03-10 10:49:07 +0530

    Update index.jsx

--- src/components/Experience/index.jsx
+++ src/components/Experience/index.jsx
@@ -34,48 +34,50 @@ const Experience = () => {
               {experiences.map((experience, index) => (
                 <ExperienceCard key={index} experience={experience} />
               ))}
-              <VerticalTimelineElement
-                contentStyle={{
-                  background: '#eaeaec',
-                  color: '#292929',
-                  boxShadow:
-                    'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-                  display: 'flex',
-                  justifyContent: 'center',
-                  alignItems: 'center',
-                }}
-                contentArrowStyle={{
-                  borderRight: '7px solid  #232631',
-                }}
-                iconStyle={{ background: '#333333' }}
-                icon={
-                  <div className="d-flex justify-content-center align-items-center w-100 h-100">
-                    <img
-                      src="icons/resume.png"
-                      alt="resume"
-                      style={{ width: '45%', height: '45%', objectFit: 'contain' }}
-                    />
-                  </div>
-                }
-              >
-                <Fade
-                  left={isDesktop}
-                  bottom={isMobile}
-                  duration={1000}
-                  delay={1000}
-                  distance="30px"
+              {!!resume && (
+                <VerticalTimelineElement
+                  contentStyle={{
+                    background: '#eaeaec',
+                    color: '#292929',
+                    boxShadow:
+                      'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                  }}
+                  contentArrowStyle={{
+                    borderRight: '7px solid  #232631',
+                  }}
+                  iconStyle={{ background: '#333333' }}
+                  icon={
+                    <div className="d-flex justify-content-center align-items-center w-100 h-100">
+                      <img
+                        src="icons/resume.png"
+                        alt="resume"
+                        style={{ width: '45%', height: '45%', objectFit: 'contain' }}
+                      />
+                    </div>
+                  }
                 >
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="cta-btn cta-btn--hero"
-                    href={resume}
-                    download
+                  <Fade
+                    left={isDesktop}
+                    bottom={isMobile}
+                    duration={1000}
+                    delay={1000}
+                    distance="30px"
                   >
-                    MY RESUME{' '}
-                  </a>
-                </Fade>
-              </VerticalTimelineElement>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn--hero"
+                      href={resume}
+                      download
+                    >
+                      MY RESUME{' '}
+                    </a>
+                  </Fade>
+                </VerticalTimelineElement>
+              )}
             </VerticalTimeline>
           </div>
         </section>
